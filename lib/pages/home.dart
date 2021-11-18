b66f7843c70b81f4e65210cc0d3e012e2c9ac6ad
mr-chidex <mbahchiboy2@gmail.com>
2021-11-18 05:35:21 +0100

    added night and day background image to home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,40 +12,55 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     //get data from loading route
     data = ModalRoute.of(context).settings.arguments;
+
+    //set background
+    bool isTime = data["isTime"];
+    String bgImg = isTime ? 'day.jpg' : "night1.jpg";
+    Color bgColor = isTime ? Colors.brown[300] : Colors.grey[700];
+    Color txtColor = isTime ? Colors.black : Colors.white;
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-          child: Column(
-            children: [
-              ElevatedButton.icon(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/$bgImg"), fit: BoxFit.cover),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            child: Column(
+              children: [
+                ElevatedButton.icon(
                   onPressed: () {
                     Navigator.pushNamed(context, '/location');
                   },
                   icon: Icon(Icons.edit_location),
-                  label: Text("Edit Location")),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data["location"],
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      letterSpacing: 2.0,
+                  label: Text("Edit Location"),
+                  style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.white,
+                      primary: bgColor,
+                      textStyle: TextStyle(fontSize: 18)),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data["location"],
+                      style: TextStyle(
+                          fontSize: 30.0, letterSpacing: 2.0, color: txtColor),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Text(data["time"],
-                  style: TextStyle(
-                    fontSize: 65.0,
-                    letterSpacing: 2.0,
-                  )),
-            ],
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(data["time"],
+                    style: TextStyle(
+                        fontSize: 65.0, letterSpacing: 2.0, color: txtColor)),
+              ],
+            ),
           ),
         ),
       ),
